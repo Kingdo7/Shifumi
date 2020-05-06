0caf9f5ce940e36b3b43dee5b4127de9244feb2c
Retro Gaming Test <kingdommangas@gmail.com>
2020-05-06 09:40:10 +0200

    Base HTML FAIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReglesComponent } from './regles/regles.component';
 import { MainsComponent } from './mains/mains.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReglesComponent,
-    MainsComponent
+    MainsComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule
