6ac14f0607d14149b5921e1ef676d003e66932b4
Retro Gaming Test <kingdommangas@gmail.com>
2020-05-06 09:29:15 +0200

    ajout composant pere-fils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReglesComponent } from './regles/regles.component';
+import { MainsComponent } from './mains/mains.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReglesComponent,
+    MainsComponent
   ],
   imports: [
     BrowserModule
